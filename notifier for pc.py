64d7202acf099b2fed062043715db64698dfdffc
Sai Pavan Saketh <64682070+saketh33@users.noreply.github.com>
2020-09-15 01:10:08 +0530

    Add files via upload

---
+++ notifier for pc.py
@@ -0,0 +1,8 @@
+from pynotifier import Notification
+#also install win10toast (pip install win10toast --if you pip)
+Notification(title='alert',
+             description="If you don't start coding you'll get fu**ed up bit**",
+             icon_path=r'C:\\Users\\saketh\\Downloads\\code-red-color-motivation.ico',  #if you jpg file convert to icon
+             duration=5,
+             urgency=Notification.URGENCY_CRITICAL
+             ).send()
