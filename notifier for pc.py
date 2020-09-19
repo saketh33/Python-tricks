7a5d65303a60baeaca19477f2c55b51c7510fd5b
Sai Pavan Saketh <64682070+saketh33@users.noreply.github.com>
2020-09-19 09:07:41 +0530

    Update notifier for pc.py

--- notifier for pc.py
+++ notifier for pc.py
@@ -1,7 +1,7 @@
 from pynotifier import Notification
 #also install win10toast (pip install win10toast --if you use pip)
 Notification(title='alert',
-             description="If you don't start coding you'll get fu**ed up bit**",
+             description="If you don't start coding you'll get wasted",
              icon_path=r'C:\\Users\\saketh\\Downloads\\code-red-color-motivation.ico',  #if you jpg file convert to icon
              duration=5,
              urgency=Notification.URGENCY_CRITICAL
